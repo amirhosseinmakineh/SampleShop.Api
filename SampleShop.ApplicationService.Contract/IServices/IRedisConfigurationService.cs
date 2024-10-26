e701c123a768c9c55973c307229a8533d4b35aa6
amirhosseinmakineh <amirhosseinmakineh1379@gmail.com>
2024-10-26 13:04:18 -0700

    Feat : Craete Simple Redis

---
+++ SampleShop.ApplicationService.Contract/IServices/IRedisConfigurationService.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleShop.ApplicationService.Contract.IServices
+{
+    public interface IRedisConfigurationService<T> where T :class
+    {
+        T GetData(string key);
+        bool SetData<T>(string key, T value, DateTimeOffset expirationTime);
+        object RemoveData(string key);
+    }
+}
---
+++ SampleShop.InfraStracture/Context/SampleShopDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SampleShop.InfraStracture.Context
+{
+    public class SampleShopDbContext:DbContext
+    {
+        public SampleShopDbContext(DbContextOptions<SampleShopDbContext> options):base(options)
+        {
+        }
+
+    }
+}
